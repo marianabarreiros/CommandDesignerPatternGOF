6a20554cf373fda4449ce9766c6148b13f1a1434
Mariana Barreiros <maridepp1@hotmail.com>
2018-04-19 15:33:48 -0300

    retirada do pacote client

--- src/commanddesignerpatterngof/CommandDesignerPatternGOF.java
+++ src/commanddesignerpatterngof/CommandDesignerPatternGOF.java
@@ -5,17 +5,36 @@
  */
 package commanddesignerpatterngof;
 
+import command.Command;
+import concretecommand.DepositeCommand;
+import concretecommand.WithDrawCommand;
+import involker.UndoMeneger;
+import java.util.Stack;
+import receiver.Account;
+
 /**
  *
  * @author Win-7
  */
 public class CommandDesignerPatternGOF {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+   public static void main(String[] args) {
+        Account account = new Account("1234", 2.000);
+        Command deposite = new DepositeCommand(account, 400);
+        Command withdraw = new WithDrawCommand(account, 600);
+        
+        UndoMeneger meneger = new UndoMeneger();
+        meneger.execute(deposite);
+        meneger.undo();
+        meneger.redo();
+        
+        Stack<Command> undoStack = new Stack<Command>();
+	Stack<Command> redoStack = new Stack<Command>();
+        
+        undoStack = meneger.getUndoStack();
+        redoStack = meneger.getRedoStack();
+        
+        System.out.println(undoStack);
+        System.out.println(redoStack);
+   }             
+ }
